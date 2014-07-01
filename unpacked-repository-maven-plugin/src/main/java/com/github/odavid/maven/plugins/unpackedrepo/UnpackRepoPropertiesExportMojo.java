5117b375b4b424145effd9604114e763e1da625f
odavid <ohad.david@gmail.com>
2014-07-01 12:08:15 +0300

    added properties export mojo

--- unpacked-repository-maven-plugin/src/main/java/com/github/odavid/maven/plugins/unpackedrepo/UnpackRepoPropertiesExportMojo.java
+++ unpacked-repository-maven-plugin/src/main/java/com/github/odavid/maven/plugins/unpackedrepo/UnpackRepoPropertiesExportMojo.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
-
+@Mojo(name = "export-properties", defaultPhase = LifecyclePhase.INITIALIZE, requiresDependencyResolution = ResolutionScope.COMPILE, threadSafe = true)
 public class UnpackRepoPropertiesExportMojo extends AbstractUnpackRepoMojo{
 	@Parameter(defaultValue="unpack.repo.")
 	private String prefix;
