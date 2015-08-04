f15b05900d04fc35e2250034457603a62a643d37
odavid <ohad.david@gmail.com>
2015-08-05 02:45:19 +0300

    trying mumbo jumbo ): take 2

--- attach-report-maven-plugin/src/it/simple-it/verify.groovy
+++ attach-report-maven-plugin/src/it/simple-it/verify.groovy
@@ -1,6 +1,5 @@
-//File indexFile = new File( basedir, "target/site/foo-report/preconfigured-report.html" );
-//File subDirFile = new File( basedir, "target/site/foo-report/ooo/test-subdir.txt" );
+File indexFile = new File( basedir, "target/site/foo-report/preconfigured-report.html" );
+File subDirFile = new File( basedir, "target/site/foo-report/ooo/test-subdir.txt" );
 
-//assert indexFile.isFile()
-//assert subDirFile.isFile()
-println 'ok'
+assert indexFile.isFile()
+assert subDirFile.isFile()
